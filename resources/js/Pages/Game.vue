05e407183d3c9b3d75a4ef855199701e0ed013ae
Alex <lulzsecer@gmail.com>
2025-08-01 14:07:06 +0300

    total clicks per game, user field status

--- resources/js/Pages/Game.vue
+++ resources/js/Pages/Game.vue
@@ -1,36 +1,40 @@
 <template>
   <Head title="Memory Game" />
-  <div class="p-4 max-w-4xl mx-auto text-center">
+  <div class="p-4 max-w-7xl mx-auto text-center">
     <h1 class="text-2xl font-bold mb-4">🧠 Memory Game</h1>
 
-    <!-- Сообщения -->
+    <!-- Messages -->
     <div
       v-if="message"
       class="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 bg-white/90 shadow-lg rounded-xl px-6 py-3 mb-4 text-lg font-semibold text-blue-700 border border-blue-200 transition-all"
     >
       {{ message }}
     </div>
-
-    <!-- Игровое поле -->
-    <div class="grid grid-cols-4 gap-2">
-      <div
-        v-for="(card, index) in cards"
-        :key="index"
-        class="bg-gray-200 rounded-xl aspect-square flex items-center justify-center text-3xl cursor-pointer select-none hover:bg-gray-300"
-        @click="flipCard(index)"
-      >
-        {{ card.flipped || card.matched ? card.value : '❓' }}
-      </div>
-    </div>
-
-    <!-- Кнопка рестарта -->
-    <div class="mt-6">
-      <button
-        class="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-xl"
-        @click="startNewGame"
-      >
-        🔁 Restart Game
-      </button>
+    <div class="flex justify-center h-screen">
+        <!-- Game field -->
+        <div class="grid grid-cols-4 gap-2">
+            <div
+                v-for="(card, index) in cards"
+                :key="index"
+                class="bg-gray-200 rounded-xl aspect-square flex items-center justify-center text-3xl cursor-pointer select-none hover:bg-gray-300"
+                @click="flipCard(index)"
+            >
+                {{ card.flipped || card.matched ? card.value : '❓' }}
+            </div>
+        </div>
+        <!-- Game status -->
+        <div class="ml-4 mt-4">
+            <p v-if="isGameWon" class="text-green-600 font-bold text-lg">🏁 You won the game!</p>
+            <p v-if="total_clicks">Total clicks: {{ total_clicks }}</p>
+            
+            <button
+            class="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-xl"
+            @click="startNewGame">
+            🔁 Restart Game
+            </button>
+        </div>
+        
+        
     </div>
   </div>
 </template>
@@ -52,6 +56,7 @@ function shuffle(array) {
 
 const cards = ref([])
 const message = ref('')
+const total_clicks = ref(0)
 const isGameWon = ref(false)
 
 onMounted(() => {
@@ -66,6 +71,8 @@ function startNewGame() {
       matched: false,
     }))
   )
+
+  total_clicks.value = 0
   message.value = ''
   isGameWon.value = false
   firstCardIndex = null
@@ -78,6 +85,7 @@ function flipCard(index) {
   if (card.flipped || card.matched) return
 
   card.flipped = true
+  total_clicks.value++
 
   if (firstCardIndex === null) {
     firstCardIndex = index
@@ -106,7 +114,9 @@ function checkWin() {
     message.value = '🏁 You won the game!'
   }
   else{
-    message.value = ''
+    setTimeout(() => {
+      message.value = ''
+    }, 2000)
   }
 }
 </script>
